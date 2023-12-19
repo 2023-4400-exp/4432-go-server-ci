75c4b4848cad919d561e1c5d2dfb048ac802d124
kknmt <m20109@g.metro-cit.ac.jp>
2023-12-19 13:58:38 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/", Hello())
+    e.GET("/api/", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.String(http.StatusOK, "4432: Hello ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4432", "message": "Hello"})
+    }
+}
