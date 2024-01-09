003fab4096009a090aacfcfc64a273df32b080dc
kknmt <m20109@g.metro-cit.ac.jp>
2024-01-09 15:06:03 +0900

    modified:   main.go

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4432", "message": "bonjour"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4432", "message": "Ciao"})
     }
 }
